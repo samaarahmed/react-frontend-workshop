c7eae5dc583aa36d9835380aba912850cdd1e64c
samaarahmed <m.samaar@gmail.com>
2022-05-03 10:38:55 +0200

    Entered data using usestate

---
+++ src/DataTable.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import { useState,useEffect } from 'react';
+
+
+const DataTable = () => {
+    const initDataForStudent = [
+        {id: "1", firstname:"Samaar", lastname: "Ahmed",age:"35",
+    birthdate:"851119",country:"Sweden",city:"Växjö"},
+    {id: "2", firstname:"Arib", lastname: "Ahmed",
+    age:"5",birthdate:"171119",country:"Sweden",city:"Växjö"}
+
+];
+
+    const [students,setStudent]= useState(initDataForStudent);
+
+    const TableHeader = () => {
+        return(            
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>FirstName</th>
+                <th>LastName</th>
+                <th>Age</th>
+                <th>BirthDate</th>
+                <th>Country</th>
+                <th>City</th>
+    
+                <th>ACTION</th>
+            </tr>
+        </thead>
+        );
+    }
+    
+    const TableAction = () =>{
+        return(
+            <div>
+                <button className='btn btn-primary'>Details</button>
+            </div>
+    
+        );
+    }
+    const TableRow = () =>{
+        return(
+    <tbody>
+        {
+          students.map(student=>
+        <tr key={student.id}>
+            <td>{student.id}</td>
+            <td>{student.firstname}</td>
+            <td>{student.lastname}</td>
+            <td>{student.age}</td>
+            <td>{student.birthdate}</td>
+            <td>{student.country}</td>
+            <td>{student.city}</td>
+    
+    
+            <td>
+                <TableAction/>
+            </td>
+        </tr>)}
+    </tbody>
+    
+        );
+    }
+    
+    return (
+        <div className='container'>
+            <span>Student list</span>
+            <table className='table table-striped'>
+
+            <TableHeader/>
+            <TableRow/>
+
+</table>
+
+        </div>
+    );
+};
+
+export default DataTable;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css'
+import DataTable from './DataTable';
 
-const msg = <h1>Hello to react</h1>
-ReactDOM.render(msg,document.getElementById('workshop'));
+
+//const msg = <h1>Hello to react world</h1>
+ReactDOM.render(<DataTable/>,document.getElementById('workshop'));
 
 
